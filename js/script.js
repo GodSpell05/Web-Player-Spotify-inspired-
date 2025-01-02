f9e7759033073aaef7458e1f8cc00a7ea368994a
Amresh <163446489+GodSpell05@users.noreply.github.com>
2025-01-02 19:19:18 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function formatTime(seconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`${folder}/`);
+  let a = await fetch(`/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -85,7 +85,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums(params) {
-  let a = await fetch(`songs/`);
+  let a = await fetch(`/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
